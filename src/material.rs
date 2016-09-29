e499869b4059b261bee0dd3bc88a743a20e6637d
nathanfaucett <nathanfaucett@gmail.com>
2016-09-28 21:04:31 -0500

    updates

--- src/material.rs
+++ src/material.rs
@@ -46,4 +46,70 @@ impl Material {
             })
         }
     }
+
+    pub fn get_shader(&self) -> Option<Shader> {
+        match self.data.shader {
+            Some(ref shader) => Some(shader.clone()),
+            None => None,
+        }
+    }
+    pub fn set_shader(&mut self, shader: Shader) -> &mut Self {
+        self.data.shader = Some(shader);
+        self
+    }
+
+    pub fn get_cull_face(&self) -> CullFace {
+        self.data.cull_face
+    }
+    pub fn set_cull_face(&mut self, cull_face: CullFace) -> &mut Self {
+        self.data.cull_face = cull_face;
+        self
+    }
+
+    pub fn get_blending(&self) -> Blending {
+        self.data.blending
+    }
+    pub fn set_blending(&mut self, blending: Blending) -> &mut Self {
+        self.data.blending = blending;
+        self
+    }
+
+    pub fn get_wireframe(&self) -> bool {
+        self.data.wireframe
+    }
+    pub fn set_wireframe(&mut self, wireframe: bool) -> &mut Self {
+        self.data.wireframe = wireframe;
+        self
+    }
+
+    pub fn get_wireframe_line_width(&self) -> f32 {
+        self.data.wireframe_line_width
+    }
+    pub fn set_wireframe_line_width(&mut self, wireframe_line_width: f32) -> &mut Self {
+        self.data.wireframe_line_width = wireframe_line_width;
+        self
+    }
+
+    pub fn get_receive_shadow(&self) -> bool {
+        self.data.receive_shadow
+    }
+    pub fn set_receive_shadow(&mut self, receive_shadow: bool) -> &mut Self {
+        self.data.receive_shadow = receive_shadow;
+        self
+    }
+
+    pub fn get_cast_shadow(&self) -> bool {
+        self.data.cast_shadow
+    }
+    pub fn set_cast_shadow(&mut self, cast_shadow: bool) -> &mut Self {
+        self.data.cast_shadow = cast_shadow;
+        self
+    }
+
+    pub fn get_uniforms(&self) -> &BTreeMap<String, usize> {
+        &self.data.uniforms
+    }
+    pub fn get_mut_uniforms(&mut self) -> &mut BTreeMap<String, usize> {
+        &mut self.data.uniforms
+    }
 }
